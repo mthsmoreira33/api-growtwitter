96fc8781f74f92cb1c10e169e16e32aee8f0c26c
Matheus Moreira <mthsmoreira59@gmail.com>
2024-02-25 14:29:51 -0300

    first commit

---
+++ prisma/migrations/20240225172909_create_model/migration.sql
@@ -0,0 +1,62 @@
+-- CreateEnum
+CREATE TYPE "TweetType" AS ENUM ('TWEET', 'RETWEET');
+
+-- CreateTable
+CREATE TABLE "twitters" (
+    "id" UUID NOT NULL,
+    "content" VARCHAR(200) NOT NULL,
+    "tweet_type" "TweetType" NOT NULL,
+
+    CONSTRAINT "twitters_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "followers" (
+    "followerId" UUID NOT NULL,
+
+    CONSTRAINT "followers_pkey" PRIMARY KEY ("followerId")
+);
+
+-- CreateTable
+CREATE TABLE "retweets" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "tweet_id" UUID NOT NULL,
+
+    CONSTRAINT "retweets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "likes" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "tweet_id" UUID NOT NULL,
+
+    CONSTRAINT "likes_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "followers" ADD CONSTRAINT "followers_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "retweets" ADD CONSTRAINT "retweets_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "retweets" ADD CONSTRAINT "retweets_tweet_id_fkey" FOREIGN KEY ("tweet_id") REFERENCES "twitters"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_tweet_id_fkey" FOREIGN KEY ("tweet_id") REFERENCES "twitters"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
